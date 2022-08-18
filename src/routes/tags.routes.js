dd9ca98cb079ec3d74a762ca69202650cbc9ff0e
Igor Fernandes <igor@alfainternet.net>
2022-08-18 11:57:15 -0300

    token users

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const TagsControllers = require("../controllers/TagsControllers");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const tagsRoutes = Router();
+
+const tagsControllers = new TagsControllers();
+
+tagsRoutes.get("/", ensureAuthenticated, tagsControllers.index);
+
+module.exports = tagsRoutes;
