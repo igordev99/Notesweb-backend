31e8dab4ee79abda722bb1f2645ee919ff749a55
Igor Fernandes <igor@alfainternet.net>
2022-08-31 10:34:38 -0300

    ..

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations")
@@ -36,6 +37,6 @@ app.use(( error, request, response, next ) => {
 
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
